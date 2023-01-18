fd2084e565723c3cbe240db881408b3fc17bc6b2
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-01-18 22:38:10 +0300

    feat(object filter): `filterName` and default filters

--- src/main/kotlin/com/aliernfrog/laclib/data/LACMapObjectFilter.kt
+++ src/main/kotlin/com/aliernfrog/laclib/data/LACMapObjectFilter.kt
@@ -1,8 +1,8 @@
 package com.aliernfrog.laclib.data
 
 data class LACMapObjectFilter(
+    val filterName: String,
     var query: String = "",
     var caseSensitive: Boolean = true,
-    var exactMatch: Boolean = true,
-    val labelStringId: Int? = null
+    var exactMatch: Boolean = true
 )
--- src/main/kotlin/com/aliernfrog/laclib/util/Constants.kt
+++ src/main/kotlin/com/aliernfrog/laclib/util/Constants.kt
@@ -1,3 +1,26 @@
 package com.aliernfrog.laclib.util
 
+import com.aliernfrog.laclib.data.LACMapObjectFilter
+
 val ILLEGAL_ROLE_CHARS = listOf(",",":")
+
+val DEFAULT_MAP_OBJECT_FILTERS = listOf(
+    LACMapObjectFilter(
+        filterName =  "Trigger box",
+        query = "Trigger_Box_Editor",
+        caseSensitive = true,
+        exactMatch = true
+    ),
+    LACMapObjectFilter(
+        filterName = "Team-deathmatch spawnpoint",
+        query = "Team_",
+        caseSensitive = true,
+        exactMatch = false
+    ),
+    LACMapObjectFilter(
+        filterName = "Racing checkpoint",
+        query = "Checkpoint_Editor",
+        caseSensitive = true,
+        exactMatch = false
+    )
+)
