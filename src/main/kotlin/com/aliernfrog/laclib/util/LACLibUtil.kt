c2ee8a944a03dcf05ddcb2269fc7b1437ee44c87
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-01-18 22:56:16 +0300

    docs: comments for LACLibUtil methods

--- src/main/kotlin/com/aliernfrog/laclib/util/LACLibUtil.kt
+++ src/main/kotlin/com/aliernfrog/laclib/util/LACLibUtil.kt
@@ -5,11 +5,19 @@ import com.aliernfrog.laclib.enum.LACMapOldObject
 
 class LACLibUtil {
     companion object {
+        /**
+         * Gets [LACMapLineType] of [line].
+         * @return [LACMapLineType] if known type, [LACMapLineType.UNKNOWN] otherwise
+         */
         fun getEditorLineType(line: String): LACMapLineType {
             return LACMapLineType.values().filter { !it.ignoreWhenFiltering }
                 .find { it.matches(line) } ?: LACMapLineType.UNKNOWN
         }
 
+        /**
+         * Finds [LACMapOldObject] for given [line].
+         * @return [LACMapOldObject] if a replacement is found, null otherwise
+         */
         fun findReplacementForObject(line: String): LACMapOldObject? {
             val objectName = line.split(":")[0]
             return LACMapOldObject.values().find { it.objectName == objectName }
