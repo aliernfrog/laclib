5421b70a1277a9d4584c6c50f6d74d1c9d182532
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-01-20 21:31:58 +0300

    refactor(LACMapObjectFilter): make filterName nullable

--- src/main/kotlin/com/aliernfrog/laclib/data/LACMapObjectFilter.kt
+++ src/main/kotlin/com/aliernfrog/laclib/data/LACMapObjectFilter.kt
@@ -1,8 +1,8 @@
 package com.aliernfrog.laclib.data
 
 data class LACMapObjectFilter(
-    val filterName: String,
     val query: String = "",
     val caseSensitive: Boolean = true,
-    val exactMatch: Boolean = true
+    val exactMatch: Boolean = true,
+    val filterName: String? = null
 )
